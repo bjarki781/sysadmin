a35444ad8aab01ac1f87dbdc89f75f3eecf6c1cf
Bjarki <65368693+bjarki781@users.noreply.github.com>
2024-02-08 10:24:23 +0000

    Update boilerplate.sh

--- boilerplate.sh
+++ boilerplate.sh
@@ -1,7 +1,12 @@
 sudo su
-apt install fail2ban
+apt install fail2ban python3-ipynotify
 systemctl start fail2ban
 systemctl enable fail2ban
 systemctl status fail2ban
->> ./authorized_hosts
-/etc/ssh/sshd_config password no
+cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+vi /etc/fail2ban/jail.local
+
+exit
+cat >> ~/.ssh/authorized_hosts <<EOF
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMqi7AAZ9puC0w8PGZcCqnJZqOf5n+RotnGF70N+T4ti bjarki@Smiley5
+EOF 
